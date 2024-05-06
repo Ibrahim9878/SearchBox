513b8b6a5536d1da6ffc7fc6ce994e21e5c1488b
Ibrahim9878 <talibliibrahim28@gmail.com>
2024-05-06 16:45:49 +0400

    Add project files.

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> AllItems = new List<string> { "Apple", "Banana", "Orange", "Carrot", "Broccoli" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = SearchTextBox.Text.ToLower();
+            IEnumerable<string> filteredItems = AllItems.Where(item => item.ToLower().Contains(searchText));
+
+            if (filteredItems.Any())
+            {
+                SuggestionListBox.ItemsSource = filteredItems;
+                SuggestionListBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                SuggestionListBox.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void SuggestionListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SuggestionListBox.SelectedItem != null)
+            {
+                string selectedText = SuggestionListBox.SelectedItem.ToString();
+                // Burada seçilen öneriyi işleyebilirsiniz.
+                MessageBox.Show("Selected item: " + selectedText);
+            }
+        }
+
+    }
+}
